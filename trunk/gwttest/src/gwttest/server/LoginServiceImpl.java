cdf6b33cdb6b6c56b61cde994d0c14e4942c7fc0
jdropmann <jdropmann@310a3696-bb1b-11de-b52b-63f31fbada87>
2009-11-03 18:31:48 +0000

    Greeting Beispiel entfernt, dafür Google LoginService Test hinzugefügt

---
+++ trunk/gwttest/src/gwttest/server/LoginServiceImpl.java
@@ -0,0 +1,39 @@
+package gwttest.server;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import gwttest.client.LoginInfo;
+import gwttest.client.LoginService;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * Implementiert den LoginService, der dazu dient den Login-Status,
+ * Userinformationen und die Login-/Logout-Uris abzurufen.
+ * @author Julian
+ */
+public class LoginServiceImpl extends RemoteServiceServlet implements
+    LoginService {
+
+	/**
+	 * @return Beinhaltet den Login-Status, Userinformationen und die Login-/Logout-Uris.
+	 * @see gwttest.client.LoginService#login(java.lang.String)
+	 * @see gwttest.client.LoginInfo
+	 */
+	public LoginInfo login(String requestUri) {
+		UserService userService = UserServiceFactory.getUserService();
+		User user = userService.getCurrentUser();
+		LoginInfo loginInfo = new LoginInfo();
+	
+		if (user != null) {
+			loginInfo.setLoggedIn(true);
+			loginInfo.setEmailAddress(user.getEmail());
+			loginInfo.setNickname(user.getNickname());
+			loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));
+	    } else {
+	    	loginInfo.setLoggedIn(false);
+	    	loginInfo.setLoginUrl(userService.createLoginURL(requestUri));
+		}
+		return loginInfo;
+	}
+}
