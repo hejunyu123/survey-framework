3706b8b6d14fdbb3920fb1dc32b93736f3ee571d
fleerkoetter <fleerkoetter@310a3696-bb1b-11de-b52b-63f31fbada87>
2009-12-15 21:12:31 +0000

    Beispielprojekt Update mit Versions - Diesmal komplett

---
+++ gwttest/src/gwttest/client/presenter/DefaultEventBus.java
@@ -0,0 +1,12 @@
+package gwttest.client.presenter;
+
+import com.google.gwt.event.shared.HandlerManager;
+
+public class DefaultEventBus extends HandlerManager implements EventBus {
+
+	public DefaultEventBus(Object source) {
+		super(source);
+		// TODO Auto-generated constructor stub
+	}
+
+}
