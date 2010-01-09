c1d1a460b10a8da1fecf3bd4a83f9b149db5a2db
jdropmann <jdropmann@310a3696-bb1b-11de-b52b-63f31fbada87>
2010-01-09 11:31:36 +0000

    added model classes, except gui-elements

---
+++ trunk/gwttest/src/gwttest/client/samplesurvey/model/Question.java
@@ -0,0 +1,66 @@
+package gwttest.client.samplesurvey.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.jdo.annotations.Extension;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.Inheritance;
+import javax.jdo.annotations.InheritanceStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION)
+@Inheritance(strategy = InheritanceStrategy.SUBCLASS_TABLE)
+public abstract class Question implements Serializable {
+	private static final long serialVersionUID = -3032677572801693979L;
+
+	@Persistent
+	private List<Answer> answers;
+
+	@Persistent
+	private String description;
+
+	@Persistent
+	private Boolean isOptional;
+
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	@Extension(vendorName = "datanucleus", key = "gae.encoded-pk", value = "true")
+	private String key;
+
+	public List<Answer> getAnswers() {
+		return answers;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Boolean getIsOptional() {
+		return isOptional;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setAnswers(List<Answer> answers) {
+		this.answers = answers;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setIsOptional(Boolean isOptional) {
+		this.isOptional = isOptional;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+}
--- trunk/gwttest/src/gwttest/client/samplesurvey/model/Survey.java
+++ trunk/gwttest/src/gwttest/client/samplesurvey/model/Survey.java
@@ -1,58 +1,124 @@
 package gwttest.client.samplesurvey.model;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
+import javax.jdo.annotations.Extension;
 import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
 public class Survey implements Serializable {
-    /**
-	 * Generated serial ID
-	 */
 	private static final long serialVersionUID = 881287580798933082L;
-	
-	@PrimaryKey
-    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
-    private String name;	
-	
+
 	@Persistent
 	private String caption;
-	
+
+	@Persistent
+	private Date createdAt;
+
 	@Persistent
 	private String description;
-	
+
+	@Persistent
+	private List<SurveyElement> elements;
+
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	@Extension(vendorName = "datanucleus", key = "gae.encoded-pk", value = "true")
+	private String key;
+
+	@Persistent
+	private String name;
+
+	@Persistent
+	private Date publishedAt;
+
+	@Persistent
+	private List<SurveyResponse> responses;
+
+	@Persistent
+	private SurveyUser user;
+
 	public Survey() {
-		caption = "";
 		description = "";
 	}
-	
+
 	public Survey(String description, String caption) {
 		this.description = description;
 		this.caption = caption;
 	}
-	
-	public void setDescription(String description) {
-		this.description = description;
+
+	public String getCaption() {
+		return caption;
 	}
-	
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
 	public String getDescription() {
 		return description;
 	}
-	
+
+	public List<SurveyElement> getElements() {
+		return elements;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Date getPublishedAt() {
+		return publishedAt;
+	}
+
+	public List<SurveyResponse> getResponses() {
+		return responses;
+	}
+
+	public SurveyUser getUser() {
+		return user;
+	}
+
 	public void setCaption(String caption) {
 		this.caption = caption;
 	}
-	
-	public String getCaption() {
-		return caption;
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setElements(List<SurveyElement> elements) {
+		this.elements = elements;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String getName() {
-		return name;
+	public void setPublishedAt(Date publishedAt) {
+		this.publishedAt = publishedAt;
+	}
+
+	public void setResponses(List<SurveyResponse> responses) {
+		this.responses = responses;
+	}
+
+	public void setUser(SurveyUser user) {
+		this.user = user;
 	}
 }
