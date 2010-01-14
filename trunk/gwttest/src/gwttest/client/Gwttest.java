54b4e10efe34d50c5466e5b60d2ab9fd0758dcf2
pesco.root <pesco.root@310a3696-bb1b-11de-b52b-63f31fbada87>
2010-01-14 18:59:30 +0000

    googleLogin aus r12 zum Test wieder eingefügt, web.xml entsprechend erweitert

--- trunk/gwttest/src/gwttest/client/Gwttest.java
+++ trunk/gwttest/src/gwttest/client/Gwttest.java
@@ -4,23 +4,86 @@ import gwttest.client.samplesurvey.MySampleSurveyPresenter;
 import gwttest.client.samplesurvey.SelectSurveyPresenter;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.InlineHTML;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
- * Entry point classes define <code>onModuleLoad()</code>.
+ * Das ist unsere EntryPoint Klasse.
  */
 public class Gwttest implements EntryPoint {
+        
+        private LoginInfo loginInfo = null;
+        private Anchor signInLink = new Anchor("Einloggen");
+        private Anchor signOutLink = new Anchor("Ausloggen");
+  
+        /**
+         * Alles hat einen Anfang ;)
+         */
+        public void onModuleLoad() {
+                //Login-Informationen holen
+                LoginServiceAsync loginService = GWT.create(LoginService.class);
+                loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<LoginInfo>() {
+                        public void onFailure(Throwable error) {
+                        }
+                        
+                        public void onSuccess(LoginInfo result) {
+                                loginInfo = result;
+                                if (loginInfo.isLoggedIn()) {
+                                    loadAdminPage();
+                                } else {
+                                    loadLoginPage();
+                                }
+                        }
+                });
+        }
 
-	/**
-	 * This is the entry point method.
-	 */
-	public void onModuleLoad() {	
-		// Construct pages and bind presenters to display		
-		@SuppressWarnings("unused")
-		MySampleSurveyPresenter mssp = GwttestFactory.get().createMySampleSurveyPage();
-		SelectSurveyPresenter selectsurvey = GwttestFactory.get().createSelectSurveyPage();
-		
-		// Show Select Survey page at startup
-		History.newItem(selectsurvey.getPlace(), true);
-	}
+        /**
+         * Lädt die GUI der Login-Seite.
+         */
+        private void loadLoginPage() {
+                //Login Panel rendern
+                signInLink.setHref(loginInfo.getLoginUrl());
+                RootPanel login = RootPanel.get("login");
+                login.add(signInLink);
+                
+                //Welcome Message rendern
+                RootPanel content = RootPanel.get("content");
+                content.add(new HTML("<h1>Easy Feedback</h1>"));
+                content.add(new InlineHTML("Hallo! Bitte "));
+                Anchor signInLink = new Anchor("loggen Sie sich ein");
+                signInLink.setHref(loginInfo.getLoginUrl());
+                content.add(signInLink);
+                content.add(new InlineHTML(", um Umfragen zu erstellen."));
+        }
+ 
+        /**
+         * Lädt die GUI der Admin-Seite.
+         */
+        private void loadAdminPage() {
+                //Login Panel rendern
+                signOutLink.setHref(loginInfo.getLogoutUrl());
+                RootPanel login = RootPanel.get("login");
+                login.add(new InlineLabel(loginInfo.getEmailAddress()));
+                login.add(new InlineLabel(" | "));
+                login.add(signOutLink);
+                
+                //Welcome Message rendern
+                RootPanel content = RootPanel.get("content");
+                content.add(new HTML("<h1>Easy Feedback</h1>"));
+                content.add(new InlineHTML("Hallo " + loginInfo.getNickname() + ", Sie haben sich erfolgreich eingeloggt!"));
+       
+                // Construct pages and bind presenters to display		
+               	@SuppressWarnings("unused")
+               	MySampleSurveyPresenter mssp = GwttestFactory.get().createMySampleSurveyPage();
+               	SelectSurveyPresenter selectsurvey = GwttestFactory.get().createSelectSurveyPage();
+
+               	// Show Select Survey page at startup
+               	History.newItem(selectsurvey.getPlace(), true);
+        }
 }
